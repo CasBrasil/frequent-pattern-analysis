2bfa4672c290b85678136f99dd8811c9f1043d6c
cfascina <cfascina@gmail.com>
2022-09-02 11:14:00 -0300

    SQL files

---
+++ queries/by-manufacturer.sql
@@ -0,0 +1,55 @@
+WITH 
+SALES AS (
+	SELECT 
+		DS.sale_code
+		,DS.barcode
+		,DSP."name" AS RO_NAME
+		,IQVIA."PRODUTO" AS IQVIA_NAME
+	FROM public.data_sale AS DS
+		LEFT JOIN data_storeproduct AS DSP ON DS.barcode = DSP.barcode
+		LEFT JOIN iqvia_produtos AS IQVIA ON LPAD(DS.barcode::VARCHAR, 15, '0') = IQVIA."EAN"
+		LEFT JOIN register_store AS RS ON DS.store_id = RS.id
+	WHERE
+		RS.retail_chain_id = 1
+		AND DS.store_id = 350
+		AND DATE BETWEEN (CURRENT_DATE - INTERVAL '5 days') AND CURRENT_DATE
+	GROUP BY 
+		DS.sale_code  
+		,DS.barcode
+		,RO_NAME
+		,IQVIA_NAME
+	ORDER BY sale_code
+)
+,FREQ AS (
+	SELECT SALES.barcode, COUNT(1) AS frequency 
+	FROM SALES
+	LEFT JOIN data_storeproduct AS DSP ON SALES.barcode = DSP.barcode 
+	WHERE DSP.factory ILIKE '%UP VITAM%'
+--	WHERE DSP.factory ILIKE ANY(ARRAY['%UP VITAM%', '%CHRYSALIS%'])
+	GROUP BY SALES.barcode
+	ORDER BY frequency DESC
+--	LIMIT 100
+)
+,BASKET AS (
+	SELECT DISTINCT SALES.sale_code
+	FROM SALES
+	INNER JOIN FREQ ON SALES.barcode = FREQ.barcode
+)
+SELECT sale_code, barcode 
+FROM (
+	SELECT
+		SALES.sale_code
+		,SALES.barcode
+		,SALES.RO_NAME
+		,SALES.IQVIA_NAME
+	FROM SALES
+	INNER JOIN BASKET ON BASKET.sale_code = SALES.sale_code
+	WHERE NOT(SALES.RO_NAME IS NULL AND SALES.IQVIA_NAME IS NULL)
+	GROUP BY 
+		SALES.sale_code
+		,SALES.barcode
+		,SALES.RO_NAME
+		,SALES.IQVIA_NAME
+	ORDER BY sale_code
+) AS SALES_BY_MANUFACTURER;
+
