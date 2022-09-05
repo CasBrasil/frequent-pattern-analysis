fd193a151fdb578cd956aa76eae44951efe6dfcf
cfascina <cfascina@gmail.com>
2022-09-05 16:09:47 -0300

    Query refactor

--- queries/by-company.sql
+++ queries/by-company.sql
@@ -1,15 +1,16 @@
-WITH
-SALES AS (
-	SELECT DS.sale_code, DS.barcode
-	FROM public.data_sale AS DS
-	LEFT JOIN register_store RS ON DS.store_id = RS.id 
+WITH 
+sales AS (
+	SELECT DS.sale_code, DS.barcode 
+	FROM data_sale DS 
+	LEFT JOIN register_store RS ON RS.id = DS.store_id
 	WHERE 
 		RS.retail_chain_id = 1
 		AND DATE BETWEEN (CURRENT_DATE - INTERVAL '1 month') AND CURRENT_DATE
+	ORDER BY DS.sale_code 
 )
 ,FREQ AS (
 	SELECT SALES.barcode, COUNT(1) AS frequency 
-	FROM SALES
+	FROM SALES 
 	GROUP BY SALES.barcode
 	ORDER BY frequency DESC
 	LIMIT 100
@@ -21,7 +22,4 @@ SALES AS (
 )
 SELECT SALES.sale_code, SALES.barcode
 FROM SALES
-INNER JOIN BASKET ON SALES.sale_code = BASKET.sale_code
-GROUP BY SALES.sale_code, SALES.barcode
-ORDER BY SALES.sale_code
-LIMIT 100;
+INNER JOIN BASKET ON BASKET.sale_code = SALES.sale_code;
