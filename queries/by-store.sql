753e413eac46fc2417f347c66eeaf0fb479d398a
cfascina <cfascina@gmail.com>
2022-09-06 11:40:49 -0300

    Store analysis

--- queries/by-store.sql
+++ queries/by-store.sql
@@ -1,16 +1,17 @@
-WITH
+WITH 
 SALES AS (
-	SELECT DS.sale_code, DS.barcode
-	FROM public.data_sale AS DS
-	LEFT JOIN register_store RS ON DS.store_id = RS.id 
+	SELECT DS.sale_code, DS.barcode 
+	FROM data_sale DS 
+	LEFT JOIN register_store RS ON RS.id = DS.store_id
 	WHERE 
 		RS.retail_chain_id = 1
 		AND DS.store_id = 350 
-		AND DATE BETWEEN (CURRENT_DATE - INTERVAL '1 month') AND CURRENT_DATE
+		AND DATE BETWEEN (CURRENT_DATE - INTERVAL '6 MONTHS') AND CURRENT_DATE
+	ORDER BY DS.sale_code 
 )
 ,FREQ AS (
 	SELECT SALES.barcode, COUNT(1) AS frequency 
-	FROM SALES
+	FROM SALES 
 	GROUP BY SALES.barcode
 	ORDER BY frequency DESC
 	LIMIT 100
@@ -22,6 +23,4 @@ SALES AS (
 )
 SELECT SALES.sale_code, SALES.barcode
 FROM SALES
-INNER JOIN BASKET ON SALES.sale_code = BASKET.sale_code
-GROUP BY SALES.sale_code, SALES.barcode
-ORDER BY SALES.sale_code;
+INNER JOIN BASKET ON BASKET.sale_code = SALES.sale_code;
